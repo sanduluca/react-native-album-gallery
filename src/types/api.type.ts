df0e38d769f5e9a8fd248d25c18c0b3f4c895a76
Sandu Luca <sanduluca1@gmail.com>
2022-06-16 00:57:31 +0300

    created api service

---
+++ src/types/api.type.ts
@@ -0,0 +1,13 @@
+export type Photo = {
+    albumId: number
+    id: number
+    title: string
+    url: string
+    thumbnailUrl: string
+}
+
+export type Album = {
+    userId: number
+    id: number
+    title: string
+}
