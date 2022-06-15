3fdcd71e78dc452ad0a5929a84f2034adc119ea0
Sandu Luca <sanduluca1@gmail.com>
2022-06-16 00:58:50 +0300

    created `Copyright` & `LoadingIndicator` component

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function Copyright() {
+    return (
+        <View style={{
+            flex: 1,
+            maxHeight: 48,
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: 4,
+            marginTop: 16
+        }}>
+            <Text>&copy; Sandu Luca</Text>
+        </View>
+    );
+}
---
+++ src/components/LoadingIndicator.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const LoadingIndicator = () => <Text style={{ color: 'white' }} >Loading..</Text>
+
+export default LoadingIndicator;
